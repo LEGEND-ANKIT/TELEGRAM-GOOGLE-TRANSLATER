34dadc3318b0fd1ea3fe82e130e905a31a8c34d4
Malitha Mihiranga <106463065+xMalitha@users.noreply.github.com>
2022-07-01 10:04:09 +0530

    Update ankit.py

--- ankit.py
+++ ankit.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🔰DEVELOPER🔰" ,url="https://github.com/LEGEND-ANKIT") ],               [InlineKeyboardButton("🔰©LEGEND-ANKIT™🔰", url="https://github.com/LEGEND-ANKIT") ]   ]  ) )
+	await message.reply_text(text =f"Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot \n I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("Developer 👨‍💻" ,url="https://t.me/xMalitha") ],               [InlineKeyboardButton("Support ✨", url="https://t.me/EpicBotsSl") ]   ]  ) )
                   
 
 
